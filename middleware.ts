e52decca8fdfcd029f424e5604f91c41267c729b
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2026-03-12 12:30:00 -0300

    fixed issues & updated consalud ui to v45

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 
+const MAINTENANCE_MODE = false; // Cambia a true para activar mantenimiento
+
 export function middleware() {
+  if (!MAINTENANCE_MODE) {
+    return NextResponse.next();
+  }
+
   return new NextResponse("Maintenance", { status: 503 });
 }
 
